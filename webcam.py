a88e863283864b952bba49b49af6526c0a9eee36
keyrun584 <89588350+keyrun584@users.noreply.github.com>
2021-09-13 19:52:24 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,12 +1,17 @@
+#video recording using ipwebcam
+
+#keyrun
+
+#importing urllib.request will get the content in the specific url
 import urllib.request
 import cv2
 import numpy as np
-
-url='http://192.168.43.1:8080/shot.jpg'
+#paste the image address url below
+url=''
 while True:
 	imgresp=urllib.request.urlopen(url)
 	imgN=np.array(bytearray(imgresp.read()),dtype=np.uint8)
-	img=cv2.imdecode(imgN,-1)
-	cv2.imshow("test",img)
-	if ord("q")==cv2.waitKey(50):
+	img=cv2.imdecode(imgN,-1) #decoding the image obtained from url
+	cv2.imshow("test",img) #displaying the image 
+	if ord("q")==cv2.waitKey(10): #if you press q it exits from the while loop hence video tab will be closed
 		exit(0)
